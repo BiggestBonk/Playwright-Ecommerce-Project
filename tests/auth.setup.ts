1076da6f69eadf294b06b93c7351b7576f8a0d0f
Harry <harry.walsh303@gmail.com>
2026-01-21 16:53:50 +1300

    initial commit

---
+++ tests/auth.setup.ts
@@ -0,0 +1,16 @@
+import {test as setup, expect} from '@playwright/test'
+import path from 'path'
+const authFile = path.join(__dirname, '../playwright/.auth/automationuser.json')
+
+setup('Amazon User Authentication', async ({page}) => {
+await page.goto('https://www.amazon.com')
+await page.getByRole('link', { name: 'Hello, sign in Account & Lists' }).click();
+await page.getByRole('textbox', { name: 'Enter mobile number or email' }).fill(process.env.USER_EMAIL!)
+await page.getByRole('button', {name:'Continue'}).click()
+await page.getByRole('textbox', {name: 'Password'}).fill(process.env.USER_PASSWORD!)
+await page.getByRole('button', {name:'Sign in'}).click()
+await page.waitForURL('amazon.com/')
+
+await page.context().storageState({path: authFile})
+})  
+    
