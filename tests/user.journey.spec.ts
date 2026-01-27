733e647be435c7bc53abe9f04960d47962e6b9f7
Harry <harry.walsh303@gmail.com>
2026-01-27 16:51:43 +1300

    fully automate user journey (up to payment)

--- tests/user.journey.spec.ts
+++ tests/user.journey.spec.ts
@@ -2,6 +2,15 @@ import { test,expect } from "@playwright/test";
 
 test.describe('User Purchase Journey',() => {
 
+    test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.amazon.com.au/cart?ref_=sw_gtc')
+    if (await page.getByText('delete').isVisible()) {
+        await page.getByText('delete').click()
+    }
+    else    
+        return
+});
+
     test('user should search, filter, add a product to cart and set delivery address', async ({page}) => { 
         
         //Navigate to Amazon (authenticated)
@@ -14,7 +23,7 @@ test.describe('User Purchase Journey',() => {
 
         //Filter and select product
         await page.getByRole('link', { name: 'Craft', exact: true }).click()
-        await page.getByRole('link', { name: 'Caydo 200 PCS Pipe Cleaners' }).click()
+        await page.locator('.s-product-image-container').first().click()
         
         //Add product to cart and checkout
         await page.getByRole('button', { name: 'Add to cart', exact: true }).click();
@@ -22,9 +31,9 @@ test.describe('User Purchase Journey',() => {
 
         //Set delivery details
         await page.getByRole('link', { name:'Add a new delivery address'}).click()
-        await page.locator('span').filter({hasText:'Australia'}).click()
-        await page.locator('#a-popover-8').getByRole('option', { name: 'New Zealand' }).click()
-        await page.getByRole('textbox', { name: 'Full name (First and Last name)' }).fill('Greg Jeffries')
+        await page.locator('span').filter({hasText:'Australia'}).nth(4).click()
+        await page.locator('#address-ui-widgets-countryCode-dropdown-nativeId_159').click();
+        await page.getByRole('textbox', { name: 'Full name (First name and surname)' }).fill('Greg Jeffries')
         await page.getByRole('textbox', { name: '9 309 6677' }).fill('2 135 9821')
         await page.getByRole('textbox', { name: 'Street address' }).fill('73 Fairytale Way')
         await page.getByRole('textbox', {name: 'Postcode'}).fill('9999')
